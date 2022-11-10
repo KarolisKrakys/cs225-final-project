5e260dabc1c7b25306040930fa7bac17dfdea912
Krakys <kkrak2@illinois.edu>
2022-11-10 17:52:36 -0600

    Added a documents folder and updated graph functionality

---
+++ graph.cpp
@@ -0,0 +1,19 @@
+#include "graph.h"
+
+Graph::Graph(std::string file_vertex, std::string file_connections){ 
+    // to implement making the entire graph
+}
+
+void Graph::insertVertex(std::string name){
+    Vertex new_vertex(name);
+    std::vector<Edge*> new_list;
+    adjlist.insert({new_vertex, new_list});
+}
+void Graph::insertEdge(Vertex v1, Vertex v2) { // v1 will point to v2;
+    Edge* to_push = new Edge(v1, v2);
+    adjlist[v1].push_back(to_push);
+}
+
+bool Graph::areAdjacent(Vertex v1, Vertex v2) {
+    return true;
+}
