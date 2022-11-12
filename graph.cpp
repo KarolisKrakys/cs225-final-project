41f34c68339aaa2826006d6a3e821512051202ad
Krakys <kkrak2@illinois.edu>
2022-11-11 20:10:32 -0600

    finished implementing all the function for graph.h

--- graph.cpp
+++ graph.cpp
@@ -1,11 +1,26 @@
 #include "include/graph.h"
 
-Graph::Graph(std::string file_vertex, std::string file_connections){ 
-    f=file_vertex;
-    f2=file_connections;
+Graph::Graph(const std::string& file_vertex, const std::string& file_connections){ 
+    std::ifstream vertices(file_vertex);
+    std::ifstream edges(file_connections);
+
+    std::string line;
+
+    while(vertices >> line) 
+        insertVertex(line);
+
+    std::istringstream iss;
+    for(std::string connection; std::getline(edges, connection);) {
+        iss.clear();
+        iss.str(connection);
+        std::string v1, v2;
+        if(iss >> v1 >> v2) {
+            insertEdge(v1, v2);
+        }
+    }
 }
 
-void Graph::insertVertex(std::string name){
+void Graph::insertVertex(const std::string& name){
     Vertex new_vertex("name");
     std::vector<Edge*> new_list;
     adjlist.insert({new_vertex, new_list});
@@ -15,6 +30,13 @@ void Graph::insertEdge(Vertex v1, Vertex v2) { // v1 will point to v2;
     adjlist[v1].push_back(to_push);
 }
 
-bool Graph::areAdjacent(Vertex v1, Vertex v2) const {
+bool Graph::areAdjacent(const Vertex& v1, const Vertex& v2) const { // does v1 point to v2
+    for(int i = 0; i < adjlist.at(v1).size(); i++) {
+        if(adjlist.at(v1).at(i)->destination_ == v2)
+            return true;
+    }
     return true;
+}
+std::vector<Graph::Edge*> Graph::incidentEdges(const Vertex& v) {
+    return adjlist.at(v);
 }
--- include/graph.h
+++ include/graph.h
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <iostream>
 #include <algorithm>
+#include <sstream>
 
 // bones of the graph class
 typedef std::string Vertex;
@@ -12,24 +13,23 @@ typedef std::string Vertex;
 class Graph {
     public:
         Graph();
-        Graph(std::string file_vertex, std::string file_connections);
+        Graph(const std::string& file_vertex, const std::string& file_connections);
         struct Edge {
             Edge(Vertex d1, Vertex o1){
                 destination_=d1;
                 origin_=o1;
             };
+            double weight = 1.0;
             Vertex destination_;
             Vertex origin_;
             bool operator==(const Edge& other) const {
                 return (other.destination_==destination_ && origin_==other.destination_);
             };
         };
-        void insertVertex(std::string name);
-        void insertEdge(Vertex v1, Vertex v2); // v1 will point to v2;
-        //std::vector<Edge*> incidentEdges(Vertex v) const;
-        bool areAdjacent(Vertex v1, Vertex v2) const;
+        void insertVertex(const std::string& name);
+        void insertEdge(const Vertex v1, const Vertex v2); // v1 will point to v2;
+        std::vector<Edge*> incidentEdges(const Vertex& v);
+        bool areAdjacent(const Vertex& v1, const Vertex& v2) const;
     private:
-        std::string f;
-        std::string f2;
         std::unordered_map<Vertex, std::vector<Edge*>> adjlist;
 };
