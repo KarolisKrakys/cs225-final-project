a4049d0347cbd96b26619056de79f0c32010c403
shikhardube03 <shikhardube2003@gmail.com>
2022-12-12 16:19:39 -0600

    add functionality with cmake but tests.cpp still wont compile

---
+++ build/CMakeFiles/cs225-final.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/shikhar/Desktop/cs225-final-project/graph.cpp" "CMakeFiles/cs225-final.dir/graph.cpp.o" "gcc" "CMakeFiles/cs225-final.dir/graph.cpp.o.d"
+  "/Users/shikhar/Desktop/cs225-final-project/main.cpp" "CMakeFiles/cs225-final.dir/main.cpp.o" "gcc" "CMakeFiles/cs225-final.dir/main.cpp.o.d"
+  "/Users/shikhar/Desktop/cs225-final-project/tests.cpp" "CMakeFiles/cs225-final.dir/tests.cpp.o" "gcc" "CMakeFiles/cs225-final.dir/tests.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/cs225-final.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cs225-final.dir/graph.cpp.o"
+  "CMakeFiles/cs225-final.dir/graph.cpp.o.d"
+  "CMakeFiles/cs225-final.dir/main.cpp.o"
+  "CMakeFiles/cs225-final.dir/main.cpp.o.d"
+  "CMakeFiles/cs225-final.dir/tests.cpp.o"
+  "CMakeFiles/cs225-final.dir/tests.cpp.o.d"
+  "cs225-final"
+  "cs225-final.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cs225-final.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
