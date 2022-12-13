d2383712250212273e47c1d44db9f6a993d5862f
KarolisKrakys <42251955+KarolisKrakys@users.noreply.github.com>
2022-12-12 23:32:39 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,20 +3,49 @@
 
 // to run use the command clang++ -std=c++20 -g -Iincludes main.cpp graph.cpp -o main
 // need to make a makefile
-int main() {
-    std::string one = "../data/articles.tsv";
-    std::string two = "../data/links.tsv";
-    Graph thing(one,two);
-    Vertex thing1("Jesus");
-    Vertex thing2("Christianity");
-    std::vector<std::string> path = thing.BFS(thing1, thing2);
-    std::cout << path.size() << std::endl;
-    for(int i = 0; i < path.size(); i++) {
-        std::cout << path[i] << " ";
+
+std::vector<std::string> argsToStrings(int argCount, char *args[]) {
+    std::vector<std::string> ret;
+    ret.resize(argCount);
+    for (size_t i = 0; i < (size_t) argCount; i++) {
+        for (size_t j = 0; j < std::strlen(args[i]); j++) {
+            ret[i] += args[i][j];
+        }            
     }
-    // for(int i = 0; i < thing.adjlist["Nintendo"].size(); i++) {
-    //     std::cout << thing.adjlist["Nintendo"][i]->origin_ << std::endl;
-    // }
-    return 0;
+    return ret;
 }
 
+
+
+int main(int argc, char *argv[]) {
+    // std::string one = "../data/articles.tsv";
+    // std::string two = "../data/links.tsv";
+    std::vector<std::string> arguments = argsToStrings(argc, argv);
+    std::transform(arguments[1].begin(), arguments[1].end(), arguments[1].begin(), [](char c){ return std::tolower(c); });
+
+    std::ifstream vertices(arguments[2]);
+    std::ifstream edges(arguments[3]);
+
+    if (!vertices.is_open() || !vertices.good()) {
+        std::cout << "Invalid File or Invalid File Path." << std::endl;
+        return 0;
+    }
+    if (!edges.is_open() || !edges.good()) {
+        std::cout << "Invalid File or Invalid File Path." << std::endl;
+        return 0;
+    }
+    Graph g(arguments[2], arguments[3]);
+    if(arguments[1] != "bfs" || arguments[1] != "pagerank" || arguments[1] != "visualize"){
+        std::cout << "The only three algorithms are: BFS, Pagerank and visualize. Please try again!";
+        return 0;
+    }
+    if(arguments[1] == "pagerank") 
+        g.computePageRank();
+    if(arguments[1] == "bfs") {
+        if(arguments[4] == arguments[5]) {
+            std::cout << "Exiting the program, starting vertex and ending vertex are the same";
+        }
+        g.BFS(arguments[4], arguments[5]);
+    }
+    return 0;
+}
