f0c1055077ff89a8ce6c1aa849d6a5d5b9b2c27d
carlosinigofernandez <cfern2@illinois.edu>
2022-12-12 19:38:45 -0600

    more test cases

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -6,7 +6,7 @@ TEST_CASE( "stupid/1=2", "Prove that one equals 2" ) {
     int one = 2;
     REQUIRE(one == 2);
 }
-TEST_CASE("Simple Path", "Two links" ) {
+TEST_CASE("Simple Path", "Two links with path size 4" ) {
     std::string one = "../data/articles.tsv";
     std::string two = "../data/links.tsv";
     Graph thing(one,two);
@@ -14,4 +14,13 @@ TEST_CASE("Simple Path", "Two links" ) {
     Vertex thing2("Emu");
     std::vector<std::string> path = thing.BFS(thing1, thing2);
     REQUIRE(path.size() == 4);
+}
+TEST_CASE("No path found", "Two links with path size 4" ) {
+    std::string one = "../data/articles.tsv";
+    std::string two = "../data/links.tsv";
+    Graph thing(one,two);
+    Vertex thing1("Lithuania");
+    Vertex thing2("Carlos");
+    std::vector<std::string> path = thing.BFS(thing1, thing2);
+    REQUIRE("No path found :(");
 }
