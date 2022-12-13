15c7a64c500acb8711524528111c36cb9fa88366
shikhardube03 <shikhardube2003@gmail.com>
2022-12-12 23:08:19 -0600

    added final test cases and implemented get max rank algo

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ int main() {
     std::string one = "../data/articles.tsv";
     std::string two = "../data/links.tsv";
     Graph thing(one,two);
-    Vertex thing1("History_of_Arizona");
-    Vertex thing2("Zulu");
+    Vertex thing1("Jesus");
+    Vertex thing2("Christianity");
     std::vector<std::string> path = thing.BFS(thing1, thing2);
     std::cout << path.size() << std::endl;
     for(int i = 0; i < path.size(); i++) {
@@ -19,3 +19,4 @@ int main() {
     // }
     return 0;
 }
+
